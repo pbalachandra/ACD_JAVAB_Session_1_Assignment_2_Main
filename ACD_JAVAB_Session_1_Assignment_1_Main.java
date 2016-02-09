973dd4dd077e1efef3a8e5c7e67f77dc75b6e938
pbalachandra <balachandra.p@gmail.com>
2016-02-09 15:02:40 +0530

    ACD_JAVAB_Session_1_Assignment_1_Main

---
+++ ACD_JAVAB_Session_1_Assignment_1_Main.java
@@ -0,0 +1,28 @@
+/**
+ * @author BALU
+ *
+ */
+		/* Write a program to print the sum of two numbers. 
+		 * One number should be int and another should be float.
+		 * Output : The sum of two numbers should be in integer.
+		 */
+public class ACD_JAVAB_Session_1_Assignment_1_Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10, sum;
+		float num2 = 10.4f;
+		
+		sum = num1 + (int)num2;
+		System.out.println("Sum : " + sum);
+	}
+
+}
+
+/* 
+* OUTPUT 
+* Sum : 20
+ */
