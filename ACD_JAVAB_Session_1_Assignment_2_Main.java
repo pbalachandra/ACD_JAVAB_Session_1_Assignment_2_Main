ecfdbbdd248f98d3431dc382a62f36d4bf8d9765
pbalachandra <balachandra.p@gmail.com>
2016-02-13 01:27:27 +0530

    ACD_JAVAB_Session_1_Assignment_2_Main

---
+++ ACD_JAVAB_Session_1_Assignment_2_Main.java
@@ -0,0 +1,19 @@
+/**
+ * @author BALU
+ *
+ */
+	/* Declare three integer variables (a,b,c) in a single declaration
+	 *  and simultaneously initialize b to 4.  */
+
+public class ACD_JAVAB_Session_1_Assignment_2_Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a, b = 4, c;
+	}
+
+}
